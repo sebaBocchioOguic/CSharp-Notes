3d90d372c792bf50aba4909d157b1e25d6e06566
sebaBocchioOguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2022-01-18 20:08:50 -0300

    Create ArithmeticsOperations.cs

---
+++ ArithmeticsOperations.cs
@@ -0,0 +1,25 @@
+// Arithmetics Operations Notes from Pedro Romanazzi Course
+
+// OPERACIONES ARITMETICAS BASICAS
+int NUM1, NUM2, RESUL;
+string linea;
+Console.Write( "PRIMER NÚMERO :" );
+linea = Console.ReadLine();
+NUM1 = int .Parse(linea);
+Console.Write( "SEGUNDO NÚMERO :" );
+linea = Console.ReadLine();
+NUM2 = int .Parse(linea);
+Console.WriteLine();
+RESUL = NUM1 + NUM2;
+Console.WriteLine( "LA SUMA ES {0}: " , RESUL);
+RESUL = NUM1 - NUM2;
+Console.WriteLine( "LA RESTA ES: {0} - {1} = {2} " , NUM1, NUM2, RESUL);
+RESUL = NUM1 * NUM2;
+Console.WriteLine( "LA MULTIPLICACIÓN ES: " + RESUL);
+RESUL = NUM1 / NUM2;
+Console.WriteLine( "LA DIVISIÓN ES: " + RESUL);
+RESUL = NUM1 % NUM2;
+Console.WriteLine( "EL RESIDUO ES: " + RESUL);
+Console.Write( "Pulse una Tecla:" );
+Console.ReadKey();
+
